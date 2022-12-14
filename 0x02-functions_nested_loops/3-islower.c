222ff92cf587e38ab27e694718972576b5678f77
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-13 18:27:00 -0800

    a function that checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,28 +8,10 @@
 int _islower(int a)
 {
 
-	a = _islower('o');
-	_putchar(a + '0');
-	a = _islower(108);
-	_putchar(a + '0');
-	a = _islower('c');
-	_putchar(a + '0');
-	a = _islower('I');
-	_putchar(a + '0');
-	a = _islower('s');
-	_putchar(a + '0');
-	a = _islower(70);
-	_putchar(a + '0');
-	a = _islower(20);
-	_putchar(a + '0');
-	a = _islower('U');
-	_putchar(a + '0');
-	a = _islower('n');
-	_putchar(a + '0');
-	a = _islower(0);
-	_putchar(a + '0');
-	a = _islower('H');
-	_putchar(a + '0');
+	if (a > 97 && a <= 122)
+	{
+		return (1);
+	}
 	_putchar('\n');
 	return (0);
 }
