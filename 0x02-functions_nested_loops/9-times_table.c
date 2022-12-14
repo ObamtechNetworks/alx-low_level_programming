961e3e66ca9d428ddfb74dd67132e8b4a93799f7
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-14 13:11:14 -0800

    9 times table starting from 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * @void: this function receives nothing
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int i, n, multiply;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (n = 1; n < 10; n++)
+		{
+			multiply = n * i;
+			if ((multiply / 10) > 0)
+			{
+				_putchar((multiply / 10) + '10');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((multiply % 10) + '10');
+			if (n < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
