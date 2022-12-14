f5a819c575ddbc134dba47ff25effa2dc7fd9ab2
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-13 19:10:50 -0800

    a function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,13 +11,15 @@ int _isalpha(int a)
 
 	if (a >= 97 && a <= 122)
 	{
-		if (a >= 65 && a <= 90)
-		{
-			if (isalpha(a))
-			{
-				return (1);
-			}
-		}
+		return (1);
+	}
+	else if (a >= 65 && a <= 90)
+	{
+		return (1);
+	}
+	else if (isalpha(a))
+	{
+		return (1);
 	}
 	return (0);
 }
