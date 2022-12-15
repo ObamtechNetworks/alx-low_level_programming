fd2e0f049db1cc35202e2f95b03aeb7884e6f935
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-14 18:11:35 -0800

    general time table 0 to 15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,5 +7,35 @@
  */
 void print_times_table(int n)
 {
+	int num, mult, res;
+
+	if (num >= 0 && num <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				res = num * multi;
+				if (res <= 99)
+					_putchar(' ');
+				if (res <= 9)
+					_putchar(' ');
+				if (res >= 100)
+				{
+					_putchar((res / 100) + '0');
+					_putchar(((res / 10)) % 10 + '0');
+				}
+				else if (res <= 99 && res >= 10)
+				{
+					_putchar((res / 10) + '0');
+				}
+				_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 	return (0);
 }
