ffafbae834e0d0e01b6a4431f6b493117c62ec61
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-14 18:30:10 -0800

    lists all natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints all natural numbers which are multiples of 3 pr 5 .
+ * @void: this function receives nothing
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; 1++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
