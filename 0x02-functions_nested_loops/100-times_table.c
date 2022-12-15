e26afd2bdb7dcf0999452e3e6a1494adc385543b
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-14 17:59:48 -0800

    prints time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - prints the time table
+ * @n: the parameter to print its time table
+ * Return: Always 0.
+ */
+void print_times_table(int n)
+{
+	return (0);
+}
