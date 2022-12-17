0a1ffca1f4b93f15ae582f0b1d98999ac6218b56
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-16 18:15:38 -0800

    draws a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - prints a square followed by a new line
+ * @size: the integer count to print square
+ * Return: 0 Always (Success)
+ */
+void print_square(int size)
+{
+	int hgt, wid;
+
+	if (size > 0)
+	{
+		for (hgt = 0; hgt < size; hgt++)
+		{
+			for (wid = 0; wid < size; wid++)
+				_putchar('#');
+			if (hgt == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
