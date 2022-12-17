437b13adcd6ebc82a5fa43d4657e0b01f19f405a
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-16 18:55:21 -0800

    The “Fizz-Buzz test” is an interview question

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point, prints the numbers from 1-100
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+			printf("FizzBuzz");
+		else if ((n % 3) == 0)
+			printf("Fizz");
+		else if ((n % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+		if (n == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
