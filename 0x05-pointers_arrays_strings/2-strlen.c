8fc48525ec5e37a42ce7bd676f766618d3814c7e
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-20 13:12:46 -0800

    a function tat returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - returns length of a string
+ * @s: the pointer variable
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
