0c1708d1234538eb54bd111e9d89a662aa1355cb
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-20 14:52:28 -0800

    print string in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,14 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int counter = 0;
+	int i, n;
 
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	for (n = (counter - 1); n >= 0; n--)
+		_putchar(s[n]);
 	_putchar('\n');
 }
