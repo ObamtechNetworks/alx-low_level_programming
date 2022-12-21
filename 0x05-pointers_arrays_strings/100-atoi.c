a997c54360b821b6ec454adcfbf2e4e0745eeacc
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-20 20:19:44 -0800

    converts a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 #include <string.h>
 #include "main.h"
 /**
@@ -12,12 +13,23 @@ int _atoi(char *s)
 	int sign = 1;
 	int i = 0;
 
-	if (s[0] == '-')
-	{
-		sign = -1;
+	while (s[i] == ' ')
 		i++;
+
+	if (s[i] == '-' || s[i] == '+')
+	{
+		sign = 1 - 2 * (s[i++] == '-');
 	}
-	for (; s[i] != '\0'; ++i)
-		res = res * 10 + s[i] - '0';
-	return (sign * res);
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (res > INT_MAX / 10 || (res == INT_MAX / 10 && s[i] - '0' > 7))
+		{
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
+		}
+		res = 10 * res + (s[i++] - '0');
+	}
+	return (res * sign);
 }
