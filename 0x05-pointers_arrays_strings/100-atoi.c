a0916ddf7d75516cbc09d47daffc43c5ba8b380d
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-20 20:57:31 -0800

    converts strings to ingers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,27 +9,16 @@
  */
 int _atoi(char *s)
 {
-	int res = 0;
 	int sign = 1;
-	int i = 0;
+	unsigned int num = 0;
 
-	while (s[i] == ' ')
-		i++;
-
-	if (s[i] == '-' || s[i] == '+')
-	{
-		sign = 1 - 2 * (s[i++] == '-');
-	}
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (res > INT_MAX / 10 || (res == INT_MAX / 10 && s[i] - '0' > 7))
-		{
-			if (sign == 1)
-				return (INT_MAX);
-			else
-				return (INT_MIN);
-		}
-		res = 10 * res + (s[i++] - '0');
-	}
-	return (res * sign);
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
 }
