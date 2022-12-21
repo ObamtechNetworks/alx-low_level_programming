ad2d3ffe972fa79e59d205d85da2a3594c4b9370
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-20 19:43:39 -0800

    a function that copies the string point to by src to dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,14 +11,14 @@ char *_strcpy(char *dest, char *src)
 	int i = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + i) != '\0')
 	{
 		i++;
 	}
 
-	for (; x < i; i++)
+	for (; x < i; x++)
 	{
-		dest[i] = src[i];
+		dest[x] = src[x];
 	}
 	dest[i] = '\0';
 	return (dest);
