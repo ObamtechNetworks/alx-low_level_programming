d3f869628934917f652c087f4834ae945b3206da
Ipadeola Bamidele Michael <ipadeolamichaelbamidele@gmail.com>
2022-12-23 15:28:49 -0800

    a function to copy portion of a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - a function that copys strings with specified amount of character
+ *
+ * @dest: string destination
+ * @src: string sourcei
+ * @n: the max number of bytes to copy from src
+ * Return: 0 Always
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0; srcLen = 0;
+
+	while (src[i++])
+		srcLen++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+	for (i = srcLen; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
+
