dcd9899134e0a83e022f853a27a81d9d9e947db2
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-14 04:55:43 +0000

    A c program that prints to std error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: receives no parameter
+ *
+ * Description: A program that prints exactly
+ * 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'
+ * to the standard error
+ *
+ * Authoer - Ipadeola Michael Bamidele
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
