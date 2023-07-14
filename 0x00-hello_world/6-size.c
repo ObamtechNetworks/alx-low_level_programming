1a78f14ff8c20269035ff9a8abaed626779bc4e7
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-14 05:31:27 +0000

    A c program that prints the size of data types on 32/64 machines

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,16 +14,10 @@
 
 int main(void)
 {
-	int a;
-	char b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("Size of a char: %zu byte(s)\n", sizeof(b));
-	printf("Size of an int: %zu byte(s)\n", sizeof(a));
-	printf("Size of a long int: %zu btye(s)\n", sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", sizeof(e));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu btye(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
