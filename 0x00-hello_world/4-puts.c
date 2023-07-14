8fb1f15256c046d2b8a51e82068ca687df7bef3c
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-14 03:08:03 +0000

    A c programm that prints to the output using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * Author - Ipadeola Michael Bamidele
- *
  * main - Entry point
- * Description - A program that prints exactly:
+ * @void: receive no parameter
+ *
+ * Description: A program that prints exactly
  * 'Programming is like building a multilingual puzzle'
  * using the puts function only
- * @void: receives no parameter
- *
+ * Authoer - Ipadeola Michael Bamidele
  * Return: Always 0 (Success)
  */
 
