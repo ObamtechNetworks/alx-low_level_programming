ccc4b6bea34a0c747698ce5d66634cc1ee359fd9
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-14 03:14:08 +0000

    A c program that prints exactly - with proper grammar, is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: receives no parameter
+ *
+ * Description: A program that prints exactly
+ * 'with proper grammar, but the outcome is a piece of art,'
+ * using the printf function only
+ *
+ * Authoer - Ipadeola Michael Bamidele
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
