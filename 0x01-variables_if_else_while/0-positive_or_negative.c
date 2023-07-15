33b233c50af1b74931af7015b60cb770944abcf6
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-15 02:37:48 +0000

    A program that generates a random value and checks if it's +ve or -ve

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* 
+ * main - Entry point
+ * Description: A program that assigns random numbers to a variable
+ * and checks if it is positive or negative
+ * @n: The integer variable that returns a random value at run time
+ * Return: Always 0 (Success) 
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
