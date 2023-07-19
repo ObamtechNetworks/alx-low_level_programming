4bcfb096156091a2592dbada0d3208127b1eaf54
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-19 05:15:40 +0000

    A function that prints the last digit of an integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,7 +4,7 @@
 /**
  * print_last_digit - prints last digit of an int
  * @number: receives any integer as argument
- * @last_digit: computed last digit of the int
+ * last_digit: computed last digit of the int
  * Description: A function that prints the
  * last digit of an integer
  * Author - Ipadeola Michael Bamidele
@@ -13,5 +13,10 @@
 
 int print_last_digit(int number)
 {
-	return(_putchar(number % 10) + '0');
+	int last_digit;
+
+	number = number % 10;
+	last_digit = _putchar(number + '0');
+
+	return (last_digit);
 }
