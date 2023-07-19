23fcc3de49fa3ed7c040b6a9e2c8da298971804b
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-19 04:56:04 +0000

    a function that prints sign of an integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - prints last digit of an int
+ * @number: receives any integer as argument
+ * @last_digit: computed last digit of the int
+ * Description: A function that prints the
+ * last digit of an integer
+ * Author - Ipadeola Michael Bamidele
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int number)
+{
+	int last_digit = _putchar(number % 10) + '0';
+	return (last_digit);
+}
