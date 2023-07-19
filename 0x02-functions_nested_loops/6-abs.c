7c8ea460e382fe2c41da70ea067ee47267434da3
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-19 04:29:47 +0000

    A function that prints the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @number: receives any integer as argument
+ * Description: A function that computes the
+ * absolute value of an integer
+ * Author - Ipadeola Michael Bamidele
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int number)
+{
+	return (abs(number));
+}
