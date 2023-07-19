f1d17c08d7650961c6e21e0db0c1521e3624b80a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-19 06:01:01 +0000

    A function that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * add - adds two integer
+ * @a: first integer argument
+ * @b: the second integer argument
+ * @result - the sum of a and b
+ * Description: A function that adds two integers
+ * and return the result
+ * Author - Ipadeola Michael Bamidele
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
