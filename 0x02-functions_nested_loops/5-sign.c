4b6db5ec8abed3d5ab733f6a91515d8c6f3513aa
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-19 04:13:18 +0000

    A function that prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: integer parameter for the function
+ * Description: A function that prints
+ * the sign of a number
+ * Author - Ipadeola Michael Bamidele
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar(43);
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar(48);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar(45);
+	}
+	return (0);
+}
