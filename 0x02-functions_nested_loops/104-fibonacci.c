f74b3bd3274fa262b7bcf5e462cb83cd96a3aed1
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-20 05:37:39 +0000

    prints the fibonacci value of first 98 numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 /**
+ * numLength - returns the length of a string
+ * @num: the integer number
+ *
+ * Return: length of number
+ */
+
+int numLength(int num)
+{
+        int length = 0;
+
+        if (!num)
+                return (1);
+        while (num)
+        {
+                num = num / 10;
+                length += 1;
+        }
+        return (length);
+}
+
+/**
 * main - Entry point
 * @void: recieves nothing
 * Description: a program that prints
@@ -11,19 +32,32 @@
 
 int main(void)
 {
-	int first = 1;
-	int second = 2;
-	int count = 98;
+	int count, init;
+	unsigned long fib1 = 1, fib2 = 2, sum,
+		      max = 100000000, fib1o = 0, fib2o = 0, sum2 = 0;
 
-	printf("%d, %d", first, second);
-	for (int i = 3; i <= count; i++)
+	for (count = 1; count <= 98; count++)
 	{
-		int next = first + second;
+		if (fib1o > 0)
+			printf("%lu", fib1o);
+		init = numLength(max) - 1 - numLength(fib1);
 
-		printf(", %d", next);
-		first = second;
-		second = next;
+		while (fib1o > 0 && init > 0)
+		{
+			printf("%d", 0);
+			init--;
+		}
+		printf("%lu", fib1o);
+		sum = (fib1 + fib2) % max;
+		sum2 = fib1o + fib2o + (fib1 + fib2) / max;
+		fib1 = fib2;
+		fib1o = fib2o;
+		fib2 = sum;
+		fib2o = sum2;
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
 	}
-	putchar('\n');
 	return (0);
 }
