dcd672109442f3295646989f46790eb770508ae3
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-20 18:37:01 +0000

    a function that adds two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Description: A function that multiplies two integers
+ * Author: Ipadeola Michael Bamidele
+ * Return: result of mulitplication
+ */
+
+int mul(int a, int b)
+{
+		return (a * b);
+}
