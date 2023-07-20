86466b5450ce2d495679ee610db25aee4ac4b185
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-20 18:48:29 +0000

    A fuction that prints numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9
+ * @void: receives nothing
+ * Description: A function that multiplies two integers
+ * Author: Ipadeola Michael Bamidele
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar(10);
+}
