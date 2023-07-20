8dd0a71272cc4a4d3b2a156eda565126d457774d
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-20 05:48:50 +0000

    fibonacci series of first 98 numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,16 +9,16 @@
 
 int numLength(int num)
 {
-        int length = 0;
+	int length = 0;
 
-        if (!num)
-                return (1);
-        while (num)
-        {
-                num = num / 10;
-                length += 1;
-        }
-        return (length);
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
 }
 
 /**
