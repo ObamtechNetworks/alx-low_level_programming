0c445583fe4a993de8fa5b65a1647f96c33732e6
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-22 04:52:18 +0000

    debugged a function that prints days remaining in a leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -29,10 +29,9 @@ void print_remaining_days(int month, int day, int year)
 	else
 	{
 		if (month == 2 && day == 60)
-			{
-				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-				
-			}
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
 		else
 		{
 			printf("day of the year: %d\n", day);
