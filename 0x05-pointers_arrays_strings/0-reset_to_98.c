ddd2f0119dd059974c5df88950d76499574a6a0d
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-24 23:19:09 +0000

    A function that receives pointer as arg and sets value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - A function that updates an int value to 98
- * @*n: a pointer to the integer value to be passed as argument
+ * @n: a pointer to the integer value to be passed as argument
  * Description: A function that sets a parameter value to 98
  * when passsed as argument to the function
  * Author - Ipadeola Michael Bamidele
