85b807271b8717d12a2edb87d5b9e1e5b551197a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-24 23:16:41 +0000

    A function that receives pointer as arg and sets value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - A function that updates an int value to 98
+ * @*n: a pointer to the integer value to be passed as argument
+ * Description: A function that sets a parameter value to 98
+ * when passsed as argument to the function
+ * Author - Ipadeola Michael Bamidele
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,9 @@
+# Now I'm Learning about pointers, arrays and strings at ALX
+### This repo would cover all the basics to advance use of pointers
+### Strings and arrays
+### Pointers arithmetic
+### Pass by reference pointers
+### Arrays Declarationg and Initialization
+### Pointer Dereferencing
+### Strings Operation and manipulation with pointers
+### And many more!
