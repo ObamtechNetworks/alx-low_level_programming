b5d252d243e44b3b0506420a5deb214b9a50920b
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-28 02:50:14 +0000

    A function that capitalize each word in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * cap_string - A fxn that captitalize each words
+ * @str : a pointer to the word string to captitalize
+ * Description: A function that capitalize words of string
+ * Author - Ipadeola Michael Bamidele
+ * Return: return pointer to the changed strings
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (ispunct(str[i]) || isblank(str[i]) || str[i] == '\n')
+		{
+			str[i + 1] = toupper(str[i + 1]);
+		}
+		i++;
+	}
+	str[i] = tolower(str[i]);
+
+	return (str);
+}
