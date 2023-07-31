194276bc03670ebe7b3b03ba783557f4c77c8d42
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-31 21:59:17 +0000

    My first commit on this repo

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @b: The constant character to fill the memory
  * @n: The most n bytes of characters to fill in
  * Description: A function that fills memory with a constant byte
- * Return a pointer to the memory area *s
+ * Return: a pointer to the memory area *s
  */
 
 char *_memset(char *s, char b, unsigned int n)
