21d7b0ba094bd4f7627bc0ff8dbf5c1490265d85
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-07-31 23:38:03 +0000

    A function that copies a memory area based on given n bytes

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _memcpy - A function that copies a memory area
+ * @dest: The destination memory location to copy to
+ * @src: The memory source to copy from
+ * @n: The most n bytes of characters to copy
+ * Description: A function that copies a memory area
+ * from source to destination based on given n bytes to copy
+ * Return: a pointer to the memory area *s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
