d260be2481b82ccdad96b1b48a2a92c58d01f1f0
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-01 06:00:36 +0000

    A function that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _strstr - A fxn that locates a substring
+ * @haystack: the main string to search from
+ * @needle: The substring to search from
+ * Description: A function that locates a substring
+ * finds the first occurene of a substring neglecting the
+ * NULL terminating bytes
+ * Return: A pointer to the beginning of the substring or
+ * NULL when no match is found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	while (needle[i] != '\0')
+	{
+		if (haystack[i] == needle[i])
+			return (needle);
+		i++;
+	}
+	return (NULL);
+}
