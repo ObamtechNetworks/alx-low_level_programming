fb7a5ac335b6215ca7516e2fb5e9257426076485
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-05 01:56:48 +0000

    created a static library, and indexed it!

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,46 +8,31 @@
  * Author - Ipadeola Michael Bamidele
  * Return: the pointer to dest
  */
-char* _atoi(char *s) {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
-    static char converted[12]; /* Assuming a maximum of 11 digits + sign */
-
-    /* Skip leading whitespaces */
-    while (s[i] == ' ') {
-        i++;
-    }
-
-    /* Check for sign */
-    if (s[i] == '-') {
-        sign = -1;
-        i++;
-    } else if (s[i] == '+') {
-        i++;
-    }
-
-    /* Convert characters to integer until a non-digit character is encountered */
-    int index = 0; /* Index for the converted array */
-    while (s[i] >= '0' && s[i] <= '9') {
-        result = result * 10 + (s[i] - '0');
-        converted[index] = s[i];
-        i++;
-        index++;
-    }
-    converted[index] = '\0'; /* Null-terminate the converted string */
-
-    /* Modify the converted string based on the sign */
-    if (sign == -1) {
-        char temp[index+1]; /* +1 for the null-terminator */
-        for (int j = 0; j < index+1; j++) {
-            temp[j] = converted[j];
-        }
-        converted[0] = '-';
-        for (int j = 0; j < index; j++) {
-            converted[j+1] = temp[j];
-        }
-    }
-
-    return converted;
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+	/* Skip leading whitespaces */
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+	/* Check for sign */
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+	/* Convert characters to integer until a non-digit character is encountered */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
