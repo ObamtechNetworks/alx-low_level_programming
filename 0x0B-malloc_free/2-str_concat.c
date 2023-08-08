f085fed2605ab67e168f7d0015b337cc1a9e2db3
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-08 10:39:40 +0000

    A function that concatenates two strings at run time

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,30 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <string.h>
 /**
- * create_array - dynamically creates an array and allocate size to it
- * @size: the size of the array to be dynamically allocated
- * @c: the character to fill in the array
- * Description: A function that dyanmically creates an array and fills it up
+ * str_concat - dynamically creates a function that concatenate two strings
+ * @s1: destination string
+ * @s2: the source string
+ * Description: A function that dyanmically concatenates two strings
  *
  * Return: NULL if size is 0, else a pointer to the array
  */
-char *create_array(unsigned int size, char c)
+char *str_concat(char *s1, char *s2)
 {
-	unsigned int i = 0;
-	char *charAr = malloc(size * sizeof(char));
-	/*handle malloc return if charAr is NULL */
-	if (charAr == NULL)
+	int size = (strlen(s1) + strlen(s2)) + 1;
+	char *ptr_join;
+	/* enough size to contain s1 and s2 */
+	char *joined = malloc(size * sizeof(char));
+
+	if (s1 == NULL)
+		/* if any of them is NULL reassign them as empty strings*/
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	/* handle malloc return */
+	if (joined == NULL)
 	{
 		return (NULL);
 		exit(1);
 	}
-	if (size == 0)
-		return (NULL);
-	while (i < size)
+	/**
+	 * create a pointer to joined, so as to keep track of concatenation
+	 * and also manipulate values directly
+	 */
+	ptr_join = joined;
+	/*fill values of s1 and s2 into ptr_join accordingly*/
+	while (*s1 != '\0')
 	{
-		*(charAr + i) = c;
-		i++;
+		/*character at s1 is stored in ptr_join*/
+		*ptr_join = *s1;
+		ptr_join++;
+		s1++;
 	}
-	return (charAr);
+	while (*s2 != '\0')
+	{
+		/*start filling in from the current position of ptr and s2*/
+		*ptr_join = *s2;
+		ptr_join++;
+		s2++;
+	}
+	/*return null pointer to end of joined string */
+	*ptr_join = '\0';
+	return (joined);
 }
