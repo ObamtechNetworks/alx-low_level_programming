9cc48efa6f5f787cd653a75ed97f29584325d956
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-08 02:14:40 +0000

    A function that dynamically allocate memory and fills it with value

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - dynamically creates an array and allocate size to it
+ * @size: the size of the array to be dynamically allocated
+ * @c: the character to fill in the array
+ * Description: A function that dyanmically creates an array and fills it up
+ *
+ * Return: NULL if size is 0, else a pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *charAr = malloc(size * sizeof(char));
+
+	while (i < size)
+	{
+		*(charAr + i) = c;
+		i++;
+	}
+	if (size == 0)
+		return (NULL);
+	return (charAr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+# Now Learning about memory allocation (malloc) and deallocation (free)
+## This concept is under the category of Dynamic Memory Allocation
+### How to dynamically allocate memory for our program during runtime
+### How to free the used memory once we are done!
