b4cbef1b2bfa9497c798c800947f6b30fafda28a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-08 02:18:24 +0000

    A function that dynamically allocate memory and fills it with value

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,17 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 	char *charAr = malloc(size * sizeof(char));
-
+	
+	/*handle malloc return if size is NULL */
+	if (size == 0)
+	{
+		return (NULL);
+		exit(1);
+	}
 	while (i < size)
 	{
 		*(charAr + i) = c;
 		i++;
 	}
-	if (size == 0)
-		return (NULL);
 	return (charAr);
 }
