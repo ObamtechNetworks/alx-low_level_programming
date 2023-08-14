96e31b3062e34126ef423d46be8f7f0a724f5b8c
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-14 20:21:06 +0000

    a fuction that initializes a struct in memoryfixed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * init_dog - a function that initializes a variable of type "struct dog"
@@ -12,6 +13,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (name == NULL)
+		name = "";
+	if (owner == NULL)
+		owner = "";
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
