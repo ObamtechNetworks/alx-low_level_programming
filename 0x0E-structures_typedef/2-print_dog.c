4c65effd3369da904b6e0b8d5deac78f90a6e6cc
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-15 04:07:22 +0000

    redefining a struct type using typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - a function that prints a "struct dog" type
+ * @d: pointer to the addrss of the struct type to print
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	/* counter */
+	int i;
+
+	/*check if d is NULL */
+	if (d == NULL)
+		exit(1);
+
+	/*print d elments */
+	for (i = 0; i < d
+
+}
