64a1c451cbea83cf6c9a4e1de91beb863d6459fe
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-25 02:56:05 +0000

    A function that runs before the main fxn

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -21,6 +21,6 @@ void execute_first_init(void)
  */
 void execute_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI
-			 bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
