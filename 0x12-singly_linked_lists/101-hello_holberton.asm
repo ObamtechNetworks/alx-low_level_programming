3d6816a8e527de0f9d33e8781e4f63eca0d230c3
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-25 12:59:41 +0000

    cleared the rax before called printf

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,13 @@
 section .data
-	format db "Hello, Holberton",0x0A ;This is the string to print, followed by a new line
+	format db "Hello, Holberton", 0x0A ;This is the string to print, followed by a new line
 
 section .text
 	extern printf
 	global main
 main:
+	xor rax, rax ;clear rax, return value to 0
 	push rbp
 	mov rdi, format
-	xor rax, rax ;Clear rax before calling printf
         call printf
 	pop rbp
 	ret
