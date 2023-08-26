b7a2d67f196a3f4e3e89772475f0019ec6292775
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-26 09:38:59 +0000

    printing opcodes

---
+++ 0x10-variadic_functions/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_opcodes - a funtion that prints opcodes of its own main function
+ *
+ *
+ *
+ */
+int print_opcodes(int main)
+{
+}
+/**
+ * main - Entry point
+ * @argc: argument counts
+ * @argv: argument vector
+ * Return: 0
+ */
+int main (int argc, char **argv)
+{
+	int n_bytes;
+
+	n_bytes = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit (1);
+	}
+
+	if (n_btes < 0)
+	{
+		printf("Eror\n");
+		exit(2);
+	return (0);
+}
