c22a653f27ed5f88f6a3d4598fbcfd8c668e5e74
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-29 23:39:47 +0000

    fixed error of not updtng link part of prev.node to be new node addr.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,13 +10,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	/*create the new node*/
 	listint_t *node_to_insert = NULL;
-
 	/*create a pointer to the head for traversing to desired index*/
 	listint_t *traverse_ptr = *head;
 
 	/*allocate space for new node in memory*/
 	node_to_insert = malloc(sizeof(listint_t));
-
 	/*handle malloc failure*/
 	if (node_to_insert == NULL)
 		return (NULL);
@@ -24,27 +22,29 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/*set data value of new node*/
 	node_to_insert->n = n;
 
+	/*check if index is 1, then if true set it to be the head*/
+	if (idx == 1)
+	{
+		node_to_insert->next = *head;
+		*head = node_to_insert;
+		return (node_to_insert);
+	}
 	/*traverse the node down to the desired position*/
 	while (idx != 1 && traverse_ptr != NULL)
 	{
 		idx--;
 		traverse_ptr = traverse_ptr->next;
 	}
-
 	/*if index is impossible, return NULL*/
-	if (idx == 0)
+	if (idx == 0 || traverse_ptr == NULL)
+	{
+		free(node_to_insert);
 		return (NULL);
-
-	/*check if index is 1, then if true set it to be the head*/
-	if (idx == 1)
-		*head = node_to_insert;
-
+	}
 	/*update the link of new node to be link of traversed pointer*/
 	node_to_insert->next = traverse_ptr->next;
-
 	/*update the link of prev. node  to be new node's addr. i.e the head*/
 	traverse_ptr->next = node_to_insert;
-
 	/*return the new_node*/
 	return (node_to_insert);
 }
