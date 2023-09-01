00148c1fac33f824b38a518be3bb7146adf306c9
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-09-01 12:22:50 +0000

    returns the place value of a bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - return the value of a bit at a given index
+ * @n: the number we want to get it bit value at particular index
+ * @index: the index of the binary bits statring from 0
+ * Return: the value of the bit at index index or -1 if an error occcured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	/*check for out of bounds or out of range index*/
+	if (index >= 32)/*when index is greater than 32bit*/
+		return (-1);
+	/*get the right most bit value, but leftshifting the 1 by index*/
+	/*then compare result by performing bitwise AND on the numb and index*/
+	/*if the result is true (set) return 1, else return 0*/
+	if (n & (1 << index))
+		return (1);
+	else
+		return (0);/*if error occured*/
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,8 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_BINARY_SIZE 64
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+char *get_binary(unsigned long int n);
+
 
 #endif /*_MAIN_H*/
