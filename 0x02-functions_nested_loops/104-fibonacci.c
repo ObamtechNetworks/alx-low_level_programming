8be0cf715646a24db95d933ad9138033b9f05674
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-09-02 21:27:29 +0000

    Revert "refactored and fixed task"
    This reverts commit 400fc74efa1dd81edec874e0eb199560b7a5bfd8.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+
+/**
+ * numLength - returns the length of a string
+ * @num: the integer number
+ *
+ * Return: length of number
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
+}
+
 /**
 * main - Entry point
 * @void: recieves nothing
@@ -11,18 +33,31 @@
 int main(void)
 {
 	int count, init;
-	unsigned long fib1 = 1, fib2 = 2, sum;
+	unsigned long fib1 = 1, fib2 = 2, sum,
+		      max = 100000000, fib1o = 0, fib2o = 0, sum2 = 0;
 
-	printf("%lu, %lu, ", fib1, fib2);
-	for (count = 2; count < 98; count++)
+	for (count = 1; count <= 98; count++)
 	{
-		sum = fib1 + fib2;
-		printf("%lu", sum);
+		if (fib1o > 0)
+			printf("%lu", fib1o);
+		init = numLength(max) - 1 - numLength(fib1);
+
+		while (fib1o > 0 && init > 0)
+		{
+			printf("%d", 0);
+			init--;
+		}
+		printf("%lu", fib1o);
+		sum = (fib1 + fib2) % max;
+		sum2 = fib1o + fib2o + (fib1 + fib2) / max;
 		fib1 = fib2;
+		fib1o = fib2o;
 		fib2 = sum;
-		if (count != 97)
+		fib2o = sum2;
+		if (count != 98)
 			printf(", ");
+		else
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
