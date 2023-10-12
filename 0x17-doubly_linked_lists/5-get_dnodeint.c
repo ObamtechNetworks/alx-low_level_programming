0914aa64f79a54e7220e5581586c0574f8035c08
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-12 22:16:17 +0000

    get node index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	/*check if index is 1, to return head as first position*/
-	if (index == 1 && head != NULL)
+	if (index == 0 && head != NULL)
 		return (head);
 	/* traverse the list, based on head and index*/
 	temp = head;
