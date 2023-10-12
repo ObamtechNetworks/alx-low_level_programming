df08f9beab592aed77b8641b6b0d329c490fef5b
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-12 22:14:30 +0000

    get node index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - retrieves a node at given index
+ * @head: pointer to the head of the node
+ * @index: the position of the node to retrieve
+ * Return: the node at that position
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	/* create a pointer to the head pointer*/
+	dlistint_t *temp = NULL;
+	/*check if head is NULL to return NULL*/
+	if (head == NULL)
+		return (NULL);
+	/*check if index is 1, to return head as first position*/
+	if (index == 1 && head != NULL)
+		return (head);
+	/* traverse the list, based on head and index*/
+	temp = head;
+	while (temp && index != 0)
+	{
+		temp = temp->next;
+		index--;
+	}
+	if (temp == NULL && index != 0)
+		return (NULL);/*index doesn't exist*/
+
+	/*else return the position*/
+	return (temp);
+}
