16e9a6521ae55d28557b2d3aa975a3d3f3e38d81
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-12 19:57:04 +0000

    number of nodes and print el in linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all elements of a doubly linked list
+ * @h: pointer to the head node
+ * Return: the numberof nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	/*creat a temp pointer for traversing*/
+	const dlistint_t *temp = NULL;
+	int count = 0; /*intialize a counter for num of nodes*/
+	/*CASE 1: head and prev of head is empty*/
+	if (h->prev == NULL && h == NULL)
+		return (0);
+	/*CASE2: if prev of head is not NULL*/
+	if (h->prev != NULL)
+	{
+		while (h->prev != NULL) /*TRAVERSE BACK TO the head*/
+			h = h->prev;
+	}
+	/*set temp to point to head*/
+	temp = h;
+	/*traverse the list now to print elements*/
+	while (temp)
+	{
+		/*print data at each position*/
+		printf("%d\n", temp->n);
+		count += 1;
+		/*move to next pointer*/
+		temp = temp->next;
+	}
+	/* return counter*/
+	return (count);
+}
