c49034059b1bcc447e865a7db208bb029f8e0ce7
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-13 01:44:29 +0000

    finally fixed the bugs!

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,23 +24,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			(*h)->prev = new_node;
 		*h = new_node;
 	}
-	else
+	temp = *h; /*traverse the list*/
+	while (i < idx - 1 && temp != NULL)
 	{
-		temp = *h;
-		for (i = 0; i < idx - 1; ++i)
-		{
-			if (temp == NULL)
-			{
-				free(new_node);
-				return (NULL);
-			}
-			temp = temp->next;
-		}
-		new_node->next = temp->next;
-		new_node->prev = temp; /*prev of temp*/
-		if (temp->next != NULL)
-			temp->next->prev = new_node;
-		temp->next = new_node;
+		temp = temp->next;
+		i++;
 	}
+	if (temp == NULL) /*index out of bound*/
+	{
+		free(new_node);
+		return (NULL);
+	}
+	/* insert at specified position*/
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	if (temp->next != NULL) /*if it's not last node*/
+		temp->next->prev = new_node;
+	temp->next = new_node;
 	return (new_node); /*return the head of the node or new node*/
 }
