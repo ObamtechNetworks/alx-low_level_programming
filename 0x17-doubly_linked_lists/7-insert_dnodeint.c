62bc7383d670ea948034e96c792b61100989716d
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-13 01:20:25 +0000

    fixed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -47,5 +47,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			temp->next->prev = new_node;
 		temp->next = new_node;
 	}
-	return (new_node); /*return the head of the node or new node*/
+	return (*h); /*return the head of the node or new node*/
 }
