18e1e28bacc9e26c21318769f205b406ad63cd5e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-03 21:13:16 +0000

    sets data for hash table

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -11,10 +11,18 @@
 int main(void)
 {
     hash_table_t *ht;
-    unsigned long int index = -1;
+    unsigned long int index = -1, index2, index3;
     ht = hash_table_create(1024);
     hash_table_set(ht, "betty", "cool");
+    
+    /*test collision*/
+    hash_table_set(ht, "hetairas", "collide");
+    hash_table_set(ht, "mentioner", "collide again");
     index = key_index((const unsigned char *)"betty", ht->size);
-    printf("key: %s, value: %s", (ht->array[index])->key, (ht->array[index])->value);
+    index2 = key_index((const unsigned char *)"mentioner", ht->size);
+    index3 = key_index((const unsigned char *)"hetairas", ht->size);
+    printf("key: %s, value: %s\n", (ht->array[index])->key, (ht->array[index])->value);
+    printf("key: %s, value: %s\n", (ht->array[index2])->key, (ht->array[index2])->value);
+    printf("key: %s, value: %s\n", (ht->array[index3])->next->key, (ht->array[index3])->next->value);
     return (EXIT_SUCCESS);
 }
