1cd5e58b1f4a4a0c3a0b819767b81880b1f94cdc
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-03-16 20:18:07 +0100

    fixed code

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -17,6 +17,9 @@ int interpolation_search(int *array, size_t size, int value)
 	if (!array || !size)
 		return (-1);
 
+	pos = low + (((double)(high - low) /
+				(array[high] - array[low])) * (value - array[low]));
+
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
 		pos = low + (((double)(high - low) /
